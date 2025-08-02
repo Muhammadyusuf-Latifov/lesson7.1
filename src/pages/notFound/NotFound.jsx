9ad0799dd8d252ecdef5a7cf4ad1bb94a5d412cf
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-08-02 14:23:21 +0500

    addNotFound

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>
+        <p className="text-[100px] text-center text-red-600 font-bold">404</p>
+        <p className="text-[24px] text-center mb-[18px]">
+          Ohh! this page is not available
+        </p>
+        <div className="flex items-center justify-center gap-[12px]">
+          <button
+            onClick={() => navigate("/")}
+            className="border py-[8px] px-[30px] rounded-[12px] bg-[dodgerblue] text-[#fff]"
+          >
+            Go Home
+          </button>
+          <button
+            onClick={() => navigate(-1)}
+            className="border py-[8px] px-[30px] rounded-[12px] bg-[#0ca8cf] text-[#fff]"
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default React.memo(NotFound);
